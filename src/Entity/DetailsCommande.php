dfe12e802bdb666d5c1dcad1eeac02e38b652b75
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-02-08 16:18:00 +0300

    shw id plat in detailsCommande

--- src/Entity/DetailsCommande.php
+++ src/Entity/DetailsCommande.php
@@ -22,7 +22,7 @@ class DetailsCommande extends AbstractDeletableEntity
 
     #[ORM\ManyToOne(inversedBy: 'detailsCommandes')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups('detailsCommande.restaurant','detailsCommande.show','detailsCommande.create')]
+    #[Groups('detailsCommande.restaurant','detailsCommande.show','detailsCommande.create','detailsCommande.list')]
     private ?Plat $idPlat = null;
 
     #[ORM\Column]
